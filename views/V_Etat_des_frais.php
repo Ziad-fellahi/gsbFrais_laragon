65bea36b9bead0ddbcef334944314d7daa997679
ZiadF <ZiadF@ZZ>
2025-09-09 17:30:15 +0200

    Version 1 Fonctionne

---
+++ views/V_Etat_des_frais.php
@@ -0,0 +1,9 @@
+<h1> Alan </h1>
+<p> formulaire de connexion </p>
+
+<nav class="menuLeft">
+    <ul class="menu-ul">
+        <li class="menu-item"><a href="index.php">retour</a></li>
+    </ul>
+</nav>
+<section class="content">
