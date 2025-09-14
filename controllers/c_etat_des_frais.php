2c69656b41383ce77af833ae283d478e15175b12
Ziad Fellahi <ziad.fellahi@outlook.fr>
2025-09-14 21:22:27 +0200

    laragon gsb

---
+++ controllers/c_etat_des_frais.php
@@ -0,0 +1,4 @@
+<?php
+// Ici tu peux préparer les données si besoin
+include 'views/v_sommaire.php';
+include 'views/v_etat_des_Frais.php';
---
+++ views/V_Etat_des_frais.php
@@ -0,0 +1,8 @@
+<h3> Etat de tout les frais par mois </h3>
+
+<div class="encadre">
+        <p>
+            Etat :
+
+
+        </p>
